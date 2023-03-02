a6c20e472629ff6c39c0ea128da7f20629ae58e2
PassbyJia <leibin2333@163.com>
2023-03-02 14:19:55 +0800

    接入Mybatis并使用mybatis的自动生成器

--- src/main/java/com/rainfir/App.java
+++ src/main/java/com/rainfir/App.java
@@ -1,7 +1,11 @@
 package com.rainfir;
 
+import com.rainfir.dao.UserDOMapper;
+import com.rainfir.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.rainfir"})
+@MapperScan("com.rainfir.dao")
 @RestController
-public class App 
+public class App
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home(){
-        return "Hello World!!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(2);
+        if (userDO==null){
+            return "用户不存在";
+        }else{
+            return userDO.getName();
+        }
+        //return "Hello World!!";
+
     }
 
     public static void main( String[] args )
